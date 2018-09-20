a877d75f7396242446c09956b21560b98c25e724
Sia X <sia.xiong@outlook.com>
2018-09-20 16:50:44 -0700

    Server

--- server/routes/todo.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let router = express.Router();
-
-let todoController = require('./../controllers/todo-controller'); 
-
-router.get('/', todoController.getAll); 
-router.post('/', todoController.create); 
-
-module.exports = router;
---
+++ server_/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const todo = require('./routes/to-do'); // Imports routes for the products
+
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://sxiong:siaxiong1@ds111113.mlab.com:11113/todolist';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/thehandiest', todo);
+
+let port = 1234;
+
+app.listen(port, () => {
+    console.log('Server is up and running on port numner ' + port);
+});
