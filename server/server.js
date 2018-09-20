80737e14c74a9eb186bb97917a84238d675b9d2a
Sia X <sia.xiong@outlook.com>
2018-09-20 15:32:43 -0700

    Webpack

--- server/server.js
+++ server/server.js
@@ -18,6 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/todo-list', toDoRouter);
+app.use(toDoRouter);
 
 module.exports = app;
