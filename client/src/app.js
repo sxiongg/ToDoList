3b164258f96f396326f023ceb1c88d8ee2bc80c2
Sia X <sia.xiong@outlook.com>
2018-09-20 14:11:47 -0700

    Initialize React front end

---
+++ client/src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Container from './components/container';
+
+ReactDOM.render(
+    <Container />, 
+    document.getElementById('app')
+)
