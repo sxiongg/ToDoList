56f8b50dea14d50feeb11c66f9391fd77790340e
Sia X <sia.xiong@outlook.com>
2018-09-20 18:24:52 -0700

    form

--- client/src/components/TextField.js
+++ client/src/components/TextField.js
@@ -4,29 +4,34 @@ import AddButton from './AddButton'
 class Input extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
+        this.state = {
             input: ''
-         }
+        }
     }
 
-    handleInput (e) {
+    handleInput(e) {
         this.setState({ input: e.target.value })
         // console.log('asdfghlkfg')
     }
 
-    render() { 
-        return ( 
+    render() {
+        return (
             <div id='input-container'>
-                <input 
-                    id='input' 
-                    type='text' 
-                    className='form-control' 
-                    value={this.state.input} 
-                    onChange={this.handleInput.bind(this)} />
-                <AddButton />
+                <form className='form-inline'>
+                    <div className="form-group">
+                        <input
+                            id='input'
+                            type='text'
+                            className='form-control'
+                            value={this.state.input}
+                            onChange={this.handleInput.bind(this)}
+                            style={{ width: '500px' }} />
+                        <button id='add-button' className='btn' type='submit'> Add </button>
+                    </div>
+                </form>
             </div>
-         );
+        );
     }
 }
- 
+
 export default Input;
