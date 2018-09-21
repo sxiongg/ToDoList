bd6a8b8e87c41fda5e5b47b096b2cf8964bc5f51
Sia X <sia.xiong@outlook.com>
2018-09-20 21:36:19 -0700

    files

--- server_/routes/to-do.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const todo_controller = require('../controllers/to-do');
-
-router.get('/to-dos', todo_controller.getAll);
-router.post('/to-dos', todo_controller.create);
-
-module.exports = router;
